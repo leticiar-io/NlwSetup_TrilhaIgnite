287b2d56dae732edc982e160588810e2c0912ef9
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-01-19 08:16:34 -0300

    adding icon plus on button

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,21 +1,26 @@
-import { View , Text, TouchableOpacity} from "react-native";
-import { Feather } from "@expo/vector-icons";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Plus } from 'phosphor-react-native';
+
 import colors from "tailwindcss/colors";
 
-import Logo from '../assets/logo.svg';
+import Logo from "../assets/logo.svg";
 
 export function Header() {
-    return (
-        <View className="w-full flex-row items-center justify-between">
-            <Logo />
+  return (
+    <View className="w-full flex-row items-center justify-between">
+      <Logo />
 
-        <TouchableOpacity activeOpacity={0.7} className={"flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"}>
-            <Feather name="plus" color={colors.violet[500]} size={20}/>
-       
-            <Text className="text-white ml-3 font-semibold text-base">
-                Novo hábito
-            </Text>
-        </TouchableOpacity>
-        </View>
-    )
+      <TouchableOpacity
+        activeOpacity={0.7}
+        className={
+          "flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+        }
+      >
+        <Plus color={colors.violet[500]} size={20} />
+        <Text className="text-white ml-3 font-semibold text-base">
+          Novo hábito
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
 }

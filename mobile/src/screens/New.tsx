e4655367f504995750ece7151bc6032d63759f7c
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-01-20 17:17:53 -0300

    connecting to API

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -6,10 +6,12 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import colors from "tailwindcss/colors";
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
+import { api } from "../lib/axios";
 
 const availableWeekDays = [
   "Domingo",
@@ -22,6 +24,7 @@ const availableWeekDays = [
 ];
 
 export function NewHabit() {
+  const [title, setTitle] = useState('');
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
   function handleToggleWeekDay(weekDayIndex: number) {
@@ -33,6 +36,25 @@ export function NewHabit() {
       setWeekDays((prevState) => [...prevState, weekDayIndex]);
     }
   }
+
+  async function handleCreateNewHabit() {
+    try {
+      if (!title.trim() || weekDays.length === 0) {
+        Alert.alert('Novo hábito', 'Informe o nome do hábito e escolha a periodicidade.')
+      }
+
+      await api.post('/habits', { title, weekDays })
+      
+      setTitle('');
+      setWeekDays([]);
+
+      Alert.alert('Novo hábito', 'Hábito criado com sucesso!');
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Ops', 'Não foi possível criar o novo hábito')
+    }
+  }
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView
@@ -52,6 +74,8 @@ export function NewHabit() {
           placeholder="Exercícios, dormir bem, etc..."
           placeholderTextColor={colors.zinc[400]}
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white focus:border-2 focus:border-green-600"
+          onChangeText={setTitle}
+          value={title}
         />
 
         <Text className="mt-4 mb-3 text-white font-semibold text-base">
@@ -69,6 +93,7 @@ export function NewHabit() {
         <TouchableOpacity
           className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
           activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
         >
           <Check size={20} color={colors.white} />
 
